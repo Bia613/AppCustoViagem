55b98fdf68b6667dd99473dba0dd419d30ac1bd2
Usuario <Usuario@CPD02-M01>
2024-04-15 12:27:20 -0300

    calculo do custo

--- AppCustoViagem/MainPage.xaml.cs
+++ AppCustoViagem/MainPage.xaml.cs
@@ -11,12 +11,24 @@
 
         private void btncal_Clicked(object sender, EventArgs e)
         {
+            double a = Convert.ToDouble(txtdis.Text);
+            double b = Convert.ToDouble(txtren.Text);
+            double c = Convert.ToDouble(txtcom.Text);
 
+            double resu = (a/b)*c ;
+
+            string msg = $"O custo da viagem é R${resu}";
+
+
+            DisplayAlert("Resultado", msg, "Fechar");
         }
 
         private void btnadd_Clicked(object sender, EventArgs e)
         {
-
+            double d = Convert.ToDouble(txtval.Text);
+           //double val = ;//
+            string msg = $"O custo da viagem com o pedagio é R$";
+            DisplayAlert("Resultado", msg, "Fechar");
         }
 
         private void btnped_Clicked(object sender, EventArgs e)
